0f2dc9bc6ddc03a7c82e1b444ffe7fba529e6cc0
Adithya K B <adithyakb93@gmail.com>
2024-02-05 20:09:38 +0530

    remove next auth url

--- lib/env.ts
+++ lib/env.ts
@@ -5,7 +5,6 @@ const envSchema = zod.object({
   GOOGLE_ID: zod.string().min(1),
   GOOGLE_SECRET: zod.string().min(1),
   NEXTAUTH_SECRET: zod.string().min(1),
-  NEXTAUTH_URL: zod.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
